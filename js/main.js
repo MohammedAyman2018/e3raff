3d8b9793754b04be04ba330b092a0654f8616eb8
MohammedAyman2018 <emidobibo20@gmail.com>
2019-10-21 00:35:45 +0200

    Add Carousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // add Navbar animation
 const   collapseCloseBtn = document.getElementById('close'),
         collapseOpenBtn = document.getElementById('open'),
@@ -10,16 +9,16 @@ collapseOpenBtn.onclick = (e) => {
         .x(0)
         .set('opacity', 1)
         .duration('0.7s')
-    .end();
+        .end();
 }
 
 collapseCloseBtn.onclick = (e) => {
-    e.target.style.display="none";
+    e.target.style.display = "none";
     move(collapseDiv)
         .x(window.innerWidth)
         .set('opacity', 0)
         .duration('0.7s')
-    .end();
+        .end();
 }
 
 // navbar dropdowns
@@ -29,11 +28,11 @@ const   list_collapse = document.querySelectorAll('[data-show]'),
         dropArray = Array.prototype.slice.call(dropDowns);
 
 list_collapse.forEach(list => {
-    list.onclick= () => {
+    list.onclick = () => {
         let listAttr = list.getAttribute('data-show'),
-            target_drop = dropArray.filter( drop => drop.getAttribute("data-number") == listAttr)[0],
-            else_drop = dropArray.filter( drop => drop.getAttribute("data-number") !== listAttr);
-        
+            target_drop = dropArray.filter(drop => drop.getAttribute("data-number") == listAttr)[0],
+            else_drop = dropArray.filter(drop => drop.getAttribute("data-number") !== listAttr);
+
         target_drop.classList.toggle('hidden');
         else_drop.forEach(list => list.classList.add('hidden'));
     }
@@ -44,17 +43,65 @@ const   horizentalArticles = document.getElementById('horizontal-articles'),
         verticalArticles = document.getElementById('vertical-articles'),
         horizentalBtn = document.getElementById('horizontal'),
         verticalBtn = document.getElementById('vertical');
-    
-horizentalBtn.onclick = (e) =>{
-    if(horizentalArticles.classList.contains('hidden')){
+
+horizentalBtn.onclick = (e) => {
+    if (horizentalArticles.classList.contains('hidden')) {
         horizentalArticles.classList.toggle('hidden');
         verticalArticles.classList.toggle('hidden');
     }
 }
 
-verticalBtn.onclick = (e) =>{
-    if(verticalArticles.classList.contains('hidden')){
+verticalBtn.onclick = (e) => {
+    if (verticalArticles.classList.contains('hidden')) {
         horizentalArticles.classList.toggle('hidden');
         verticalArticles.classList.toggle('hidden');
     }
 }
+
+// Cahnge showen Carousel
+const   siemas = document.querySelectorAll('.siema');
+
+for (const siema of siemas) {
+    new Siema({
+        selector: siema,
+        duration: 200,
+        easing: 'ease-in-out',
+        perPage: {
+            0: 1,
+            425: 2, // 2 items for viewport wider than 800px
+            768: 3, // 3 items for viewport wider than 800px
+            1240: 4 // 4 items for viewport wider than 1240px
+        },
+        startIndex: 0,
+        draggable: true,
+        threshold: 20,
+        loop: true,
+        rtl: true,
+    });
+    console.log('siema initilized successfully');
+};
+// const   prev = document.querySelector ('.prev');
+// const   next = document.querySelector('.next');
+
+// prev.addEventListener('click', () => mySiema.prev());
+// next.addEventListener('click', () => mySiema.next());
+
+const   toggleBtns = document.querySelectorAll('a[data-carousel-show]'),
+        carousels = document.querySelectorAll('div[data-carousel-show]'),
+        carouselsArray = Array.prototype.slice.call(carousels);
+
+toggleBtns.forEach(btn => {
+    btn.onclick = () => {
+        let btnAttr = btn.getAttribute('data-carousel-show'),
+            target_carousel = carouselsArray.filter(carousel => carousel.getAttribute("data-carousel-show") == btnAttr)[0],
+            else_carousel = carouselsArray.filter(carousel => carousel.getAttribute("data-carousel-show") !== btnAttr);
+
+            
+        target_carousel.classList.toggle('hidden');
+        else_carousel.forEach(carousel => {
+            if(carousel.classList.contains('hidden') == false){
+                carousel.classList.add('hidden')
+            }
+        })
+    }
+});
